974bcb7f01564a7fbe15c829ebcacd6591d48e41
akshaya2005 <100388070+akshaya2005@users.noreply.github.com>
2026-06-23 13:44:10 -0400

    Add: sliding-window-maximum - submission-9

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-9.py
@@ -0,0 +1,43 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        pq = []
+        res = []
+
+        class item:
+            def __init__(self, num, index):
+                self.num = num
+                self.index = index
+            
+            def __lt__(self, other):
+                return self.num < other.num
+        
+        for i in range(k):
+            heapq.heappush(pq, item(-nums[i], i))
+    
+        l = 0
+        for r in range(k, len(nums) + 1):
+            top = pq[0]
+        
+            while pq and pq[0].index <= l:
+                heapq.heappop(pq)
+            
+            res.append(-top.num)
+            if r < len(nums):
+                heapq.heappush(pq, item(-nums[r], r))
+            l += 1
+        return res
+            
+
+
+
+            
+
+        
+        
+
+
+
+
+
+
+        
