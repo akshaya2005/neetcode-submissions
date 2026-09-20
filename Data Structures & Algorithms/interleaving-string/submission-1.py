7eb6b229be17bc5e69d3134042271dc467ba05ad
akshaya2005 <100388070+akshaya2005@users.noreply.github.com>
2026-09-19 21:25:10 -0400

    Add: interleaving-string - submission-1

---
+++ Data Structures & Algorithms/interleaving-string/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n = len(s1)
+        m = len(s2)
+
+        if n + m != len(s3):
+            return False
+
+        dp = [[False for _ in range(m + 1)] for _ in range(n + 1)]
+        dp[0][0] = True
+
+        # dp[i][j] = can we make s3[:i+j] using
+        # the first i characters of s1 and the first j characters of s2?
+
+        for i in range(n + 1):
+            for j in range(m + 1):
+
+                # Take the next character from s1
+                if i > 0 and s1[i - 1] == s3[i + j - 1] and dp[i - 1][j]:
+                    dp[i][j] = True
+
+                # Take the next character from s2
+                if j > 0 and s2[j - 1] == s3[i + j - 1] and dp[i][j - 1]:
+                    dp[i][j] = True
+
+        return dp[n][m]
