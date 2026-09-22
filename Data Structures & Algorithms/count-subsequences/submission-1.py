6533c302218a2f81ceb292541ca61695e6228973
akshaya2005 <100388070+akshaya2005@users.noreply.github.com>
2026-09-22 00:02:14 -0400

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        ## dp[i, j] represents the number of ways to make 
+        ## the first j characters of t using the first 
+        ## i characters of s
+        m = len(s)
+        n = len(t)
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = 1
+
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                dp[i][j] = dp[i-1][j]
+                if s[i-1] == t[j-1]:
+                    dp[i][j] += dp[i-1][j-1]
+        print(dp)
+        return dp[m][n]
+
+
+
+        
